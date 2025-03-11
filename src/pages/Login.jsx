4ae84f57a354ee8f56db4f6a2ec6bc44c49f5889
Ömer <omareany99@hotmail.com>
2025-03-11 02:18:08 +0100

    ändra stylingen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate("")
+
     return (
-      <div>
-        <p>L</p>
+      <div className="container py-5">
+        <div className="sign-in mx-auto p-3">
+
+          <div className="text-center">
+            <h1>Logga in</h1>
+          </div>
+
+          <form action="">
+
+            <input 
+              type="email"
+              placeholder="E-post"  
+            />
+            <input 
+              type="password"
+              placeholder="Lösenord"
+            />
+
+            <button className="btn btn-primary py-2 mb-3" type="submit">Logga in</button>
+
+            <div className="form-check mb-4">
+              <p>Glömt lösenord? <NavLink to={"/reset-password"}>Återställ här</NavLink></p>
+            </div>
+
+          </form>
+
+        </div>
       </div>
     );
   }
